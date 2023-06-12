38b5d65e0726d266d1c18aae4007a421a22e3a74
DarwinDontCare <117847214+DarwinDontCare@users.noreply.github.com>
2023-06-12 01:14:41 -0300

    +changed background and changed ui style

---
+++ javascript/main_page.js
@@ -0,0 +1,25 @@
+
+
+function increase_day() {
+    
+}
+
+function decrease_day() {
+    
+}
+
+function add_todo() {
+
+}
+
+function load_todo_list() {
+
+}
+
+function store_todos() {
+
+}
+
+function get_moon_phase() {
+
+}
